4001c88386ad4e053ace415b639b820d99a27d88
RobertAKARobin <robertgfthomas@gmail.com>
2018-08-13 10:21:00 -0500

    Serving from dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
 		}),
 		new HtmlWebpackPlugin({
 			template: path.resolve(__dirname, './src/index.html'),
-			filename: path.resolve(__dirname, './index.html'),
+			filename: path.resolve(__dirname, './dist/index.html'),
 			inject: false
 		}),
 	],
