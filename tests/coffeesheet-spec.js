be245d1e236d0cf05d03bdcbcf9fcfd8bbbf9a98
RobertAKARobin <robertgfthomas@gmail.com>
2018-08-22 14:57:02 -0500

    Added tests for subtables

--- tests/coffeesheet-spec.js
+++ tests/coffeesheet-spec.js
@@ -4,14 +4,35 @@ global.document = window.document
 const o = require("mithril/ospec/ospec")
 const {Coffeesheet, Table, Section, Row, Cell} = require('../src/coffeesheet')
 
-
 o.spec('Coffeesheet', ()=>{
-	let coffeesheet =  new Coffeesheet()
+	let coffeesheet
+	o.spec('#constructor', ()=>{
+		coffeesheet = new Coffeesheet()
+		o('creates one table', (done)=>{
+			o(coffeesheet.tables.length).equals(1)
+			done()
+		})
+	})
 	o.spec('#createTable', ()=>{
-		o('increases the instance\'s tables by 1', ()=>{
+		o('increases the instance\'s tables by 1', (done)=>{
 			let numTables = coffeesheet.tables.length
 			coffeesheet.createTable()
-			o(coffeesheet.tables.length).equals(numTables + 1)
+			o(coffeesheet.tables.length).equals(2)
+			done()
+		})
+	})
+	o.spec('.tables, each', ()=>{
+		o('is a Table', (done)=>{
+			coffeesheet.tables.forEach((table)=>{
+				o(table.constructor).equals(Table)
+			})
+			done()
+		})
+		o('has at least one section', (done)=>{
+			coffeesheet.tables.forEach((table)=>{
+				o(table.sections.length >= 1).equals(true)
+			})
+			done()
 		})
 	})
 })
