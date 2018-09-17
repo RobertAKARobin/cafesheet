371ac3a5df3298748dfe9a246454d557d472bc8d
RobertAKARobin <robertgfthomas@gmail.com>
2018-09-17 14:56:53 -0500

    Bugfix for cell data being duplicated
    Don't get this bug at all... If cell.datum is undefined, it shows the datum of the previous cell?

--- src/js/cafesheet.views.js
+++ src/js/cafesheet.views.js
@@ -68,7 +68,7 @@ Cell.prototype.view = function(){
 	let cell = this
 	return m('td', [
 		m('textarea', {
-			value: cell.datum,
+			value: (cell.datum === undefined ? '' : cell.datum),
 			oninput: (event)=>{
 				event.redraw = false
 				cell.datum = event.target.value
