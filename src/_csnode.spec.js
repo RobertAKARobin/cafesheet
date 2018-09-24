bdd360736b35b11791d25f330cd4a66e306dccf7
RobertAKARobin <robertgfthomas@gmail.com>
2018-09-24 12:12:22 -0500

    Changed variable prefixes

--- src/_csnode.spec.js
+++ src/_csnode.spec.js
@@ -1,31 +1,31 @@
-const Classes = [Sheet, Table, Section, Row, Cell]
+const $Classes = [Sheet, Table, Section, Row, Cell]
 
-Classes.forEach((Class) => {
+$Classes.forEach(($Class) => {
 	'use strict'
 	
-	const parentClass = Classes[Classes.indexOf(Class) - 1],
-		childClass = Classes[Classes.indexOf(Class) + 1],
-		ancestorClasses = Classes.slice(0, Classes.indexOf(Class)).reverse(),
-		descendantClasses = Classes.slice(Classes.indexOf(Class) + 1)
+	const $parentClass = $Classes[$Classes.indexOf($Class) - 1],
+		$childClass = $Classes[$Classes.indexOf($Class) + 1],
+		$ancestorClasses = $Classes.slice(0, $Classes.indexOf($Class)).reverse(),
+		$descendantClasses = $Classes.slice($Classes.indexOf($Class) + 1)
 
-	o.spec(`${Class.name}`, ()=>{
-		let $
+	o.spec(`${$Class.name}`, ()=>{
+		let _ = {}
 
 		o.spec(`class`, ()=>{
 			o(`.parentClass`, ()=>{
-				o(Class.parentClass).equals(parentClass)
+				o($Class.parentClass).equals($parentClass)
 			})
 			o(`.childClass`, ()=>{
-				o(Class.childClass).equals(childClass)
+				o($Class.childClass).equals($childClass)
 			})
 			o(`.ancestorClasses`, ()=>{
-				let expected = ancestorClasses.map(C=>C.name).join(','),
-					actual = Class.ancestorClasses.map(C=>C.name).join(',')
+				let expected = $ancestorClasses.map(C=>C.name).join(','),
+					actual = $Class.ancestorClasses.map(C=>C.name).join(',')
 				o(actual).equals(expected)
 			})
 			o(`.descendantClasses`, ()=>{
-				let expected = descendantClasses.map(C=>C.name).join(','),
-					actual = Class.descendantClasses.map(C=>C.name).join(',')
+				let expected = $descendantClasses.map(C=>C.name).join(','),
+					actual = $Class.descendantClasses.map(C=>C.name).join(',')
 				o(actual).equals(expected)
 			})
 		})
@@ -34,30 +34,30 @@ Classes.forEach((Class) => {
 			let instance
 
 			o.beforeEach(()=>{
-				$ = {}
-				$.sheet = new Sheet()
-				$.table = $.sheet.tables[0]
-				$.section = $.table.sections[0]
-				$.row = $.section.rows[0]
-				$.cell = $.row.cells[0]
-				instance = $[Class.singularName]
+				_ = {}
+				_.sheet = new Sheet()
+				_.table = _.sheet.tables[0]
+				_.section = _.table.sections[0]
+				_.row = _.section.rows[0]
+				_.cell = _.row.cells[0]
+				instance = _[$Class.singularName]
 			})
 
 			o(`.class`, ()=>{
-				o(instance.class).equals(Class)
+				o(instance.class).equals($Class)
 			})
 			o(`.parentClass`, ()=>{
-				o(instance.parentClass).equals(parentClass)
+				o(instance.parentClass).equals($parentClass)
 			})
 			o(`.childClass`, ()=>{
-				o(instance.childClass).equals(childClass)
+				o(instance.childClass).equals($childClass)
 			})
 			o(`.parent`, ()=>{
-				o(instance.parent).equals(parentClass ? $[parentClass.singularName] : undefined)
+				o(instance.parent).equals($parentClass ? _[$parentClass.singularName] : undefined)
 			})
-			if(parentClass){
-				o(`.${parentClass.singularName}`, ()=>{
-					o(instance.parent).equals($[parentClass.singularName])
+			if($parentClass){
+				o(`.${$parentClass.singularName}`, ()=>{
+					o(instance.parent).equals(_[$parentClass.singularName])
 				})
 			}
 		})
