409b4339e7640602661ac0e2754a49894da4f54f
RobertAKARobin <robertgfthomas@gmail.com>
2018-11-06 20:28:18 -0600

    Added base.all

--- src/base.js
+++ src/base.js
@@ -1,9 +1,24 @@
 function Base(){
 	const instance = this
 	const children = []
-	const all = {}
-	all[Table] = {}
+
+	const all = new Map()
+	all.set(Table, new Set())
+	all.set(Section, new Set())
+	all.set(Row, new Set())
+	all.set(Cell, new Set())
+
 	Object.defineProperties(instance, {
+		all: {
+			get: function(){
+				return {
+					tables: Array.from(all.get(Table)),
+					sections: Array.from(all.get(Section)),
+					rows: Array.from(all.get(Row)),
+					cells: Array.from(all.get(Cell))
+				}
+			}
+		},
 		children: {
 			get: ()=>instance.tables
 		},
