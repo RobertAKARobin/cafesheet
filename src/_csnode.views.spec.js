137c72c11fba4eea7e113db36f372a4c9cfb27c9
RobertAKARobin <robertgfthomas@gmail.com>
2018-11-26 15:42:56 -0600

    Just renamed whole method to DOM

--- src/_csnode.views.spec.js
+++ src/_csnode.views.spec.js
@@ -3,27 +3,28 @@ function frame(){
 }
 
 o.spec('Cafesheet in browser', ()=>{
-	const DOM = {},
-		Data = {}
+	const Data = {}
+	const componentToDOMMapping = {
+		'bases': 'div.base',
+		'tables': 'table',
+		'sections': 'tbody',
+		'rows': 'tr',
+		'cells': 'td',
+		'celldata': 'td textarea',
+		'createButton': 'button[action=create]',
+		'removeButton': 'button[action=remove]',
+		'rowIndex': '.rowIndex'
+	}
+	function DOM(){
+		let root, selector
+		if(arguments.length > 1){
+			[root, selector] = arguments
+		}else{
+			[selector] = arguments
+		}
+		return Array.from((root || document).querySelectorAll(componentToDOMMapping[selector]))
+	}
 
-	o.before(()=>{
-		const componentToElementMapping = [
-			['bases', 'div.base'],
-			['tables', 'table'],
-			['sections', 'tbody'],
-			['rows', 'tr'],
-			['cells', 'td'],
-			['celldata', 'td textarea'],
-			['createButton', 'button[action=create]'],
-			['removeButton', 'button[action=remove]'],
-			['rowIndex', '.rowIndex']
-		]
-		componentToElementMapping.forEach(map=>{
-			DOM[map[0]] = function(root){
-				return Array.from((root || document).querySelectorAll(map[1]))
-			}
-		})
-	})
 	o.beforeEach(()=>{
 		Data.tables = Array.from(CSData.tables)
 		Data.sections = Data.tables.map(t=>t.sections).flat()
@@ -35,41 +36,41 @@ o.spec('Cafesheet in browser', ()=>{
 	})
 	o.spec('on load', ()=>{
 		o('tables', ()=>{
-			o(DOM.bases()[0].children[0].tagName.toLowerCase()).equals('table')
-			o(DOM.tables().length).equals(Data.tables.length)
+			o(DOM('bases')[0].children[0].tagName.toLowerCase()).equals('table')
+			o(DOM('tables').length).equals(Data.tables.length)
 		})
 		o('sections', ()=>{
-			o(DOM.sections().length).equals(Data.sections.length)
+			o(DOM('sections').length).equals(Data.sections.length)
 		})
 		o('rows', ()=>{
-			o(DOM.rows().length).equals(Data.rows.length)
+			o(DOM('rows').length).equals(Data.rows.length)
 
 			const dataRowIndexes = Data.rows.map(r=>Data.rows.indexOf(r))
-			const domRowIndexes = DOM.rows().map(r=>parseInt(DOM.rowIndex(r)[0].textContent))
+			const domRowIndexes = DOM('rows').map(r=>parseInt(DOM(r, 'rowIndex')[0].textContent))
 			o(domRowIndexes).deepEquals(dataRowIndexes)
 		})
 		o('cells', ()=>{
-			o(DOM.cells().length).equals(Data.cells.length)
+			o(DOM('cells').length).equals(Data.cells.length)
 		})
 		o('celldata', ()=>{
-			o(DOM.celldata().length).equals(Data.cells.length)
-			o(DOM.celldata().map(d=>d.value)).deepEquals(Data.cells.map(c=>c.datum))
+			o(DOM('celldata').length).equals(Data.cells.length)
+			o(DOM('celldata').map(d=>d.value)).deepEquals(Data.cells.map(c=>c.datum))
 		})
 	})
 	o.spec('@row', ()=>{
 		o('click create button', async ()=>{
-			o(DOM.rows().length).equals(Data.rows.length)
-			DOM.createButton(DOM.rows()[0])[0].click()
+			o(DOM('rows').length).equals(Data.rows.length)
+			DOM(DOM('rows')[0], 'createButton')[0].click()
 			await frame()
-			o(DOM.rows().length).equals(Data.rows.length + 1)
+			o(DOM('rows').length).equals(Data.rows.length + 1)
 		})
 		o('click remove button', async ()=>{
-			const firstRow = DOM.rows()[0]
-			DOM.removeButton(DOM.rows()[0])[0].click()
-			o(DOM.celldata(firstRow).map(t=>t.value)).deepEquals(Data.rows[0].cells.map(c=>c.datum))
+			const firstRow = DOM('rows')[0]
+			DOM(firstRow, 'removeButton')[0].click()
+			o(DOM(firstRow, 'celldata').map(t=>t.value)).deepEquals(Data.rows[0].cells.map(c=>c.datum))
 			await frame()
-			o(DOM.rows().length).equals(Data.rows.length - 1)
-			o(DOM.celldata(firstRow).map(t=>t.value)).deepEquals(Data.rows[1].cells.map(c=>c.datum))
+			o(DOM('rows').length).equals(Data.rows.length - 1)
+			o(DOM(firstRow, 'celldata').map(t=>t.value)).deepEquals(Data.rows[1].cells.map(c=>c.datum))
 		})
 	})
 })
