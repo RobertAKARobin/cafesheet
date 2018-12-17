8bda15de4efdbe31c0122ea9544e881280f1c17e
RobertAKARobin <robertgfthomas@gmail.com>
2018-12-17 13:23:41 -0600

    Need to use defineProperties after all, since .freeze is weird

--- src/row.js
+++ src/row.js
@@ -1,45 +1,60 @@
-const Row = {
-	ancestors: [Section, Table, Base],
-	name: 'Row',
-	parent: Section,
-	pluralName: 'rows',
-
-	create(input = {}){
-		const row = Object.create(Row.proto)
-		const pvt = {
-			children: [],
-			instance: row,
-			parent: undefined
-		}
-		if(input.cells instanceof Array){
-			pvt.children = Array.from(input.cells)
-			// TODO: Detect if raw functions are passed as children
-		}
-		if(input.parent && input.parent.constructor === Row.parent){
-			pvt.parent = input.parent
-		}
-
-		Object.assign(row, Row.generateInstanceMethods(pvt))
-		Object.freeze(row)
-		return row
-	}
-}
-Row.generateInstanceMethods = function(pvt){
-	return {
-		getChildren: Cafesheet.instance.getChildren(pvt),
-		getParent: Cafesheet.instance.getParent(pvt)
-	}
-}
-Row.proto = {
-	class: Row,
-	getWidth(){
-		return this.getChildren().length
+const Row = Object.defineProperties({}, {
+	ancestors: {
+		get: ()=>[Section, Table, Base]
 	},
-	toJSON(){
-		return {
-			cells: this.getChildren()
+	name: {
+		value: 'Row'
+	},
+	parent: {
+		get: ()=>Section
+	},
+	pluralName: {
+		value: 'rows'
+	},
+
+	proto: {
+		value: Object.defineProperties({}, {
+			class: {
+				get: ()=>Row
+			},
+			getWidth: {
+				value: function(){
+					return this.getChildren().length
+				}
+			},
+			toJSON: {
+				value: function(){
+					return {
+						cells: this.getChildren()
+					}
+				}
+			}
+		})
+	},
+
+	create: {
+		value: (input = {})=>{
+			const pvt = {
+				children: [],
+				parent: undefined
+			}
+			const row = pvt.instance = Object.create(Row.proto, {
+				getChildren: {
+					value: Cafesheet.instance.getChildren(pvt)
+				},
+				getParent: {
+					value: Cafesheet.instance.getParent(pvt)
+				}
+			})
+
+			if(input.cells instanceof Array){
+				pvt.children = Array.from(input.cells)
+				// TODO: Detect if raw functions are passed as children
+			}
+			if(input.parent && input.parent.constructor === Row.parent){
+				pvt.parent = input.parent
+			}
+			return row
 		}
 	}
-}
-Object.freeze(Row)
-Object.freeze(Row.proto)
+})
